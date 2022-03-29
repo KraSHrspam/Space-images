1acd2b1f9954124cc26874cecee9bd7adf94af31
KraSHrspam <mlevin0210@gmail.com>
2022-03-29 18:19:06 +0300

    publish text to Telegram channel.
    added folders in .gitignore

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import requests
-import os
-from urllib.parse import urlparse
 import io
+import os
+import telegram
 from pprint import pprint
+from telegram import Update
+from dotenv import load_dotenv
+from urllib.parse import urlparse
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 link = "https://api.spacexdata.com/v3/launches"
-apod_link = "https://api.nasa.gov/planetary/apod?api_key=YjSmH3pN6d3J7YGlSmNN1NdlYnXnHvmak9DpbLbV"
+apod_link = "https://api.nasa.gov/planetary/apod?count=50&api_key=YjSmH3pN6d3J7YGlSmNN1NdlYnXnHvmak9DpbLbV"
+epic_link = "https://api.nasa.gov/EPIC/api/natural/images?api_key=YjSmH3pN6d3J7YGlSmNN1NdlYnXnHvmak9DpbLbV"
+
+load_dotenv()
+token = os.getenv('TOKEN')
 
 def get_apod(apod_link):
+    apod_picture_num = 0
     response = requests.get(apod_link)
     response.raise_for_status()
-    return response.json()["hdurl"]
+    for apod_picture in response.json():
+        apod_picture_num += 1
+        try:
+            picture_download(apod_picture["url"], f"APOD_pictures/Apod{apod_picture_num}")
+        except:
+            print("Ошибка,Ошибка!")
+
+#t.me/EPIC_NASA_pictures_bot
+#5136479351:AAG7H078n17prSyg0gZNS64sL2svFJxF2UE
+
+def get_epic_pictures(epic_link):
+    epic_picture_num = 0
+    response = requests.get(epic_link)
+    response.raise_for_status()
+    for epic_picture in response.json():
+        epic_picture_num += 1
+        pic_name = epic_picture['image']
+        year = pic_name[8:12]
+        month = pic_name[12:14]
+        day = pic_name[14:16]
+        finished_epic_link = f"https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/{epic_picture['image']}.png?api_key=YjSmH3pN6d3J7YGlSmNN1NdlYnXnHvmak9DpbLbV"
+        print(finished_epic_link)
+        try:
+            picture_download(finished_epic_link, f"EPIC_pictures/EPIC{epic_picture_num}")
+        except:
+            print("ЭПИЧЕСКАЯ ОШИБКА!")
 
 def get_image_extansion(picture_link):
     parse_url = urlparse(picture_link)
@@ -35,7 +69,10 @@ def fetch_spacex_last_launch():
     pictures_of_rocket = get_spacex_pictures()
     for url in pictures_of_rocket:
         picture_num += 1
-        picture_download(url, f"SpaceX{picture_num}")
+        picture_download(url, f"SpaceX_pictures/SpaceX{picture_num}")
 
-#fetch_spacex_last_launch()
-pprint(get_apod(apod_link))
+bot = telegram.Bot(token=token)
+updates = bot.get_updates()
+bot.send_message(text='Hi Mark!', chat_id="@EPIC_NASA_pictures_group")
+
+#get_epic_pictures(epic_link)
