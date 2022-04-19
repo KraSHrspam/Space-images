94ec4036a6de06ec52f3fc5ae16039be723710b9
KraSHrspam <70641065+KraSHrspam@users.noreply.github.com>
2022-04-19 18:22:11 +0300

    Deleted test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from time import sleep
-
-while True:
-    for address, dirs, files in os.walk("EPIC_pictures"):
-        for file in files:
-            print(address+'/'+file)
-            sleep(10)
