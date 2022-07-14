e1412271dbfdc813eb2158c95daa83fe56150fa8
KraSHrspam <70641065+KraSHrspam@users.noreply.github.com>
2022-07-14 14:13:49 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -8,8 +8,8 @@ def get_image_extension(picture_link):
     extension_of_url = os.path.splitext(parse_url_path)
     return extension_of_url[1]
 
-def picture_download(link, picture_name, params=""):
+def picture_download(link, picture_path, params=""):
     response = requests.get(link, params=params)
     response.raise_for_status()
-    with open(picture_name + get_image_extension(link), 'wb') as file:
+    with open(picture_path + get_image_extension(link), 'wb') as file:
         file.write(response.content)
