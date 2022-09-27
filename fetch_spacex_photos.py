aea554b7cb3516736784ed7f913f3a7799e0ede1
KraSHrspam <70641065+KraSHrspam@users.noreply.github.com>
2022-09-27 18:08:32 +0300

    Remade SpaceX

--- fetch_spacex_photos.py
+++ fetch_spacex_photos.py
@@ -4,21 +4,18 @@ import requests
 from tools import picture_download
 
 
-def get_spacex_picture_url():
-    link = "https://api.spacexdata.com/v3/launches"
-    launch_number = os.getenv("SPACEX_LAUNCH_NUMBER")
+def fetch_spacex_last_launch(launch_id, picture_dir):
+    link = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(link)
     response.raise_for_status()
-    return response.json()[launch_number]["links"]["flickr_images"]
-
-
-def fetch_spacex_last_launch():
-    pictures_of_rocket = get_spacex_picture_url()
+    pictures_of_rocket = response.json()["links"]["flickr"]["original"]
     for picture_num, picture in enumerate(pictures_of_rocket):
+        print(f"#Загружаю СпейсИКС фотку номер {picture_num}")
         picture_name = f"SpaceX{picture_num}"
-        picture_download(picture, os.path.join())
+        picture_download(picture, os.path.join(picture_dir, picture_name))
 
 if __name__ == '__main__':
-    launch_number = os.getenv["SPACEX_LAUNCH_NUMBER"]
+    launch_id = os.getenv("SPACEX_LAUNCH_ID","5eb87d46ffd86e000604b388")
     picture_dir = "SpaceX_pictures"
     os.makedirs(picture_dir, exist_ok=True)
+    fetch_spacex_last_launch(launch_id, picture_dir)
